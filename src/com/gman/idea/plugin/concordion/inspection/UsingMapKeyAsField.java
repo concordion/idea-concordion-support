563113dcd789db0ef641d079b2185a6abd20fe6b
Ievgen Degtiarenko <gmandnepr@gmail.com>
2015-09-21 21:23:29 -0400

    Warn user if using map key as a field

---
+++ src/com/gman/idea/plugin/concordion/inspection/UsingMapKeyAsField.java
@@ -0,0 +1,26 @@
+package com.gman.idea.plugin.concordion.inspection;
+
+import com.gman.idea.plugin.concordion.lang.psi.ConcordionField;
+import com.intellij.codeInspection.LocalInspectionTool;
+import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import org.jetbrains.annotations.NotNull;
+
+public class UsingMapKeyAsField extends LocalInspectionTool {
+
+    @NotNull
+    @Override
+    public PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder holder, boolean isOnTheFly) {
+        return new PsiElementVisitor() {
+            @Override
+            public void visitElement(PsiElement element) {
+                super.visitElement(element);
+
+                if (element instanceof ConcordionField && ((ConcordionField) element).isKeyInMap()) {
+                    holder.registerProblem(element, "Using map key as a field");
+                }
+            }
+        };
+    }
+}
