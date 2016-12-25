7238024110d302b97201d5e97df53eb996abc4aa
Ievgen Degtiarenko <ievgen.degtiarenko@gmail.com>
2016-12-25 16:12:59 +0100

    New example icons

--- src/org/concordion/plugin/idea/lang/ConcordionIcons.java
+++ src/org/concordion/plugin/idea/lang/ConcordionIcons.java
@@ -1,11 +1,10 @@
 package org.concordion.plugin.idea.lang;
 
-import com.intellij.icons.AllIcons;
 import com.intellij.openapi.util.IconLoader;
 
 import javax.swing.*;
 
 public class ConcordionIcons {
     public static final Icon ICON = IconLoader.getIcon("/org/concordion/plugin/idea/lang/concordion.png");
-    public static final Icon EXAMPLE = AllIcons.Nodes.Enum;
+    public static final Icon EXAMPLE = IconLoader.getIcon("/org/concordion/plugin/idea/lang/example.png");
 }
