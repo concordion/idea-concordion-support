245330e72f478e24937a8a8e4696a80de5d7add2
Ievgen Degtiarenko <ievgen.degtiarenko@gmail.com>
2017-03-05 11:20:23 +0100

    Fixes #13

--- src/org/concordion/plugin/idea/fixtures/ConcordionScalaTestFixture.java
+++ src/org/concordion/plugin/idea/fixtures/ConcordionScalaTestFixture.java
@@ -1,5 +1,6 @@
 package org.concordion.plugin.idea.fixtures;
 
+import com.intellij.lang.Language;
 import com.intellij.psi.PsiAnnotation;
 import com.intellij.psi.PsiElement;
 import org.concordion.plugin.idea.action.quickfix.factories.ScalaFixtureMemberFactory;
@@ -7,14 +8,13 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.plugins.scala.lang.psi.api.base.ScStableCodeReferenceElement;
 import org.jetbrains.plugins.scala.lang.psi.api.expr.ScArgumentExprList;
-import org.jetbrains.plugins.scala.ScalaLanguage;
 
 import static com.intellij.psi.util.PsiTreeUtil.*;
 
 public class ConcordionScalaTestFixture extends AbstractConcordionTestFixture<ScStableCodeReferenceElement> {
 
     public ConcordionScalaTestFixture() {
-        super(ScalaLanguage.Instance, "scala", ScStableCodeReferenceElement.class, new ScalaFixtureMemberFactory());
+        super(Language.findLanguageByID("Scala"), "scala", ScStableCodeReferenceElement.class, new ScalaFixtureMemberFactory());
     }
 
     @Nullable
